475b1ca1e66146153231987ce60794d321cea208
john smith <a8961713@gmail.com>
2026-05-10 18:09:01 +0300

    Use only Ubuntu 24.04

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ DOCKER_EXE = docker
 NAME_ZEPHYR = zephyr_downloader_zephyr
 #NAME_1804 = zephyr_downloader_1804
 #NAME_2004 = zephyr_downloader_2004
-NAME_2204 = zephyr_downloader_2204
-#NAME_2404 = zephyr_downloader_2404
+#NAME_2204 = zephyr_downloader_2204
+NAME_2404 = zephyr_downloader_2404
 
 .PHONY: all build download clean rmi
 
@@ -16,15 +16,15 @@ download:
 	$(DOCKER_EXE) run -t -v $(shell pwd)/output:/artifacts $(NAME_ZEPHYR)
 #	$(DOCKER_EXE) run -t -v $(shell pwd)/output:/artifacts $(NAME_1804)
 #	$(DOCKER_EXE) run -t -v $(shell pwd)/output:/artifacts $(NAME_2004)
-	$(DOCKER_EXE) run -t -v $(shell pwd)/output:/artifacts $(NAME_2204)
-#	$(DOCKER_EXE) run -t -v $(shell pwd)/output:/artifacts $(NAME_2404)
+# 	$(DOCKER_EXE) run -t -v $(shell pwd)/output:/artifacts $(NAME_2204)
+	$(DOCKER_EXE) run -t -v $(shell pwd)/output:/artifacts $(NAME_2404)
 
 build:
 	$(DOCKER_EXE) build -f Dockerfile.zephyr --no-cache --tag $(NAME_ZEPHYR) .
 #	$(DOCKER_EXE) build -f Dockerfile.pip_1804 --no-cache --tag $(NAME_1804) .
 #	$(DOCKER_EXE) build -f Dockerfile.pip_2004 --no-cache --tag $(NAME_2004) .
-	$(DOCKER_EXE) build -f Dockerfile.pip_2204 --no-cache --tag $(NAME_2204) .
-#	$(DOCKER_EXE) build -f Dockerfile.pip_2404 --no-cache --tag $(NAME_2404) .
+# 	$(DOCKER_EXE) build -f Dockerfile.pip_2204 --no-cache --tag $(NAME_2204) .
+	$(DOCKER_EXE) build -f Dockerfile.pip_2404 --no-cache --tag $(NAME_2404) .
 
 clean:
 	$(RM) output/*.tar.bz2
--- download_zephyr.sh
+++ download_zephyr.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-west init --mr v4.3.0 zephyrproject
+west init --mr v4.4.0 zephyrproject
 cd zephyrproject
 west update
 
 # Add logging functions because error 'AttributeError: 'Blobs' object has no attribute 'dbg'' (for example) is received
-sed -i '/^class /a\ \ \ \ def dbg(self, msg):\n\ \ \ \ \ \ \ \ print(msg)' /zephyrproject/zephyr/scripts/west_commands/blobs.py 
-sed -i '/^class /a\ \ \ \ def inf(self, msg):\n\ \ \ \ \ \ \ \ print(msg)' /zephyrproject/zephyr/scripts/west_commands/blobs.py 
-sed -i '/^class /a\ \ \ \ def wrn(self, msg):\n\ \ \ \ \ \ \ \ print(msg)' /zephyrproject/zephyr/scripts/west_commands/blobs.py 
-sed -i '/^class /a\ \ \ \ def err(self, msg):\n\ \ \ \ \ \ \ \ print(msg)' /zephyrproject/zephyr/scripts/west_commands/blobs.py 
+# sed -i '/^class /a\ \ \ \ def dbg(self, msg):\n\ \ \ \ \ \ \ \ print(msg)' /zephyrproject/zephyr/scripts/west_commands/blobs.py 
+# sed -i '/^class /a\ \ \ \ def inf(self, msg):\n\ \ \ \ \ \ \ \ print(msg)' /zephyrproject/zephyr/scripts/west_commands/blobs.py 
+# sed -i '/^class /a\ \ \ \ def wrn(self, msg):\n\ \ \ \ \ \ \ \ print(msg)' /zephyrproject/zephyr/scripts/west_commands/blobs.py 
+# sed -i '/^class /a\ \ \ \ def err(self, msg):\n\ \ \ \ \ \ \ \ print(msg)' /zephyrproject/zephyr/scripts/west_commands/blobs.py 
 west blobs fetch hal_espressif
 west blobs fetch hal_stm32
 
 # Remove the functions bacuase they create an "TypeError: Blobs.dbg() got an unexpected keyword argument 'level'" later when compiling
-sed -i '/^\ \ \ \ def dbg(self, msg):$/{N; /^\ \ \ \ def dbg(self, msg):\n\ \ \ \ \ \ \ \ print(msg)$/d;}' /zephyrproject/zephyr/scripts/west_commands/blobs.py
-sed -i '/^\ \ \ \ def inf(self, msg):$/{N; /^\ \ \ \ def inf(self, msg):\n\ \ \ \ \ \ \ \ print(msg)$/d;}' /zephyrproject/zephyr/scripts/west_commands/blobs.py
-sed -i '/^\ \ \ \ def wrn(self, msg):$/{N; /^\ \ \ \ def wrn(self, msg):\n\ \ \ \ \ \ \ \ print(msg)$/d;}' /zephyrproject/zephyr/scripts/west_commands/blobs.py
-sed -i '/^\ \ \ \ def err(self, msg):$/{N; /^\ \ \ \ def err(self, msg):\n\ \ \ \ \ \ \ \ print(msg)$/d;}' /zephyrproject/zephyr/scripts/west_commands/blobs.py
+# sed -i '/^\ \ \ \ def dbg(self, msg):$/{N; /^\ \ \ \ def dbg(self, msg):\n\ \ \ \ \ \ \ \ print(msg)$/d;}' /zephyrproject/zephyr/scripts/west_commands/blobs.py
+# sed -i '/^\ \ \ \ def inf(self, msg):$/{N; /^\ \ \ \ def inf(self, msg):\n\ \ \ \ \ \ \ \ print(msg)$/d;}' /zephyrproject/zephyr/scripts/west_commands/blobs.py
+# sed -i '/^\ \ \ \ def wrn(self, msg):$/{N; /^\ \ \ \ def wrn(self, msg):\n\ \ \ \ \ \ \ \ print(msg)$/d;}' /zephyrproject/zephyr/scripts/west_commands/blobs.py
+# sed -i '/^\ \ \ \ def err(self, msg):$/{N; /^\ \ \ \ def err(self, msg):\n\ \ \ \ \ \ \ \ print(msg)$/d;}' /zephyrproject/zephyr/scripts/west_commands/blobs.py
 
 find . -name \.git|xargs rm -rf
 
